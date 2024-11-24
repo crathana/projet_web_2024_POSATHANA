a6abe80d26de3388df2629a32e1b6117520af9de
crathana <168814281+crathana@users.noreply.github.com>
2024-11-24 21:35:22 +0100

    Add files via upload

---
+++ bindpopup.php
@@ -0,0 +1,55 @@
+this.marker[8].bindPopup(
+    <p><strong> Traducteur universel </strong> </p>
+    <p>Vous avez entre les mains le traducteur dernier cri développé par © Saclay-mentPerformant! Toutefois, pour pouvoir bénéficier de toutes les fonctionnalités, il faut le dévérouiller en tapant le code secret. </p>
+    <p>Nous avons perdu la notice de cet exemplaire et ne pouvons pas vous le fournir... Par contre nous avons retrouvé un papier contenant les informations suivantes... </p>
+    <p><strong> Indice:</strong></p>
+    <p> " Vous qui avez la chance d'être un valide,</p>
+    <p>Combattez l'ennemi pleutre et félon. </p>
+    <p>Pour cela il faut qu'on corde de manière rapide</p>
+    <p>L'arc avec lequel nous triompherons.</p>
+    <p><strong>Code:</strong></p>
+)
+
+this.marker[9].bindPopup(
+    <p><strong> Tour Eiffel</strong> </p>
+    <p> L'adresse de ce monument emblématique de la France est le ... avenue Anatole France.</p>  
+)
+
+this.marker[10].bindPopup(
+    <p><strong> Les Invalides</strong> </p>
+    <p> Cette structure impressionante a été construite en seulement ... ans! </p>  
+)
+
+this.marker[11].bindPopup(
+    <p><strong> Cathédrale Notre-Dame de Paris</strong> </p>
+    <p> Dans cette cathédrale iconique de la ville de Paris, ... empereurs ont été intronisés </p>  
+)
+
+this.marker[12].bindPopup(
+    <p><strong> Arc de Triomphe</strong> </p>
+    <p> Il y a ... grands reliefs représentant des victoires militaires et des evenements historiques sculptés sur ce monument.</p>  
+)
+
+this.marker[13].bindPopup(
+    <p><strong> Musée du Louvre</strong> </p>
+    <p> Le musée comporte un total de ... portes principales !</p>  
+)
+this.marker[14].bindPopup(
+    <p><strong> Place de la Concorde</strong> </p>
+    <p> Les bassins des fontaines  mesurent environs ... mètres de diamètre.</p> 
+)
+
+this.marker[15].bindPopup(
+    <p><strong> Panthéon</strong> </p>
+    <p> Dans ce bâtiment est stocké le pendule de Foucault, qui mesure ... mètres de long environs.</p>  
+)
+
+this.marker[16].bindPopup(
+    <p><strong> Basilique du Sacré-Coeur</strong> </p>
+    <p> On peut parfois y entendre sonner ses ... cloches.</p>  
+)
+
+this.marker[17].bindPopup(
+    <p><strong> Opéra Garnier</strong> </p>
+    <p> Il a été construit avec ... types différents de marbre !</p>  
+)
